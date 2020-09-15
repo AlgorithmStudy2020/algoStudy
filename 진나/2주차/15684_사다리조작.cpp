5d234b1c817344c02de64ce77d83467c440ef2a5
jjjinna <didwlssk2002@gmail.com>
2020-09-16 01:26:24 +0900

    week2

--- 진나/2주차/15684_사다리조작.cpp
+++ 진나/2주차/15684_사다리조작.cpp
@@ -1,28 +1,54 @@
 #include<iostream>
-
-#define NMAX 10
-#define HMAX 30
-
+#define scanf scanf_s
+#define MAX 31
 using namespace std;
 
+int N, M, H;	//세로선 N개, 가로선 M개, 가로선 놓을 수 있는 위치 H개
+int a, b;	//b번 세로선과 b+1번 세로선을 a번 점선 위치에서 연결
+int input[MAX][MAX];
+int ans = -1,ladder_num;
+void init() {
+	scanf("%d %d %d", &N, &M, &H);
 
-int input[NMAX+1][HMAX+1];
-int main() {
-	int N, M, H; //세로선의 개수 N, 가로선의 개수 M, 세로선마다 가로선 놓을 수 있는 위치의 개수 H
-	cin >> N >> M >> H;
-
-	int a, b;
+	int yindex, xindex;
 	for (int i = 0; i < M; i++) {
-		cin >> a >> b;
-		input[a][b] = b + 1;
-		input[a][b + 1] = b;
+		scanf("%d %d", &yindex, &xindex);
+		input[yindex][xindex] = 1;
+	}
+}
+
+bool ladder() {
+	for (int i = 1; i <= N; i++) {	//시작점
+		int cur = i;
+		for (int j = 1; j <= H; j++) {
+			if (input[j][cur])	cur+=1;	//오른쪽으로 이동
+			else if (input[j][cur - 1])	cur -= 1;	//왼쪽으로 이동
+		}
+		if (i != cur)	return false;
 	}
 
-	/*
-	for (int i = 1; i <= H; i++) {
-		for (int j = 1; j <= M; j++) {
-			cout << input[i][j] << " ";
-		}cout << endl;
-	}*/
+	return true;
+}
 
+void dfs(int x,int cnt) {
+	if (cnt > 3)	return;	//3개 이상 놓는경우
+	//정답을 찾은 경우
+	if (ladder()) { ans = cnt;}
+	if (ans != -1)	return;
+
+	//조합 탐색
+	for (int i = x; i <= H; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (input[i][j - 1]||input[i][j+1]||input[i][j])	continue;	//가로선이 연속하면 안됨
+			input[i][j] = 1;
+			dfs(i,cnt + 1);
+			input[i][j] = 0;
+		}
+	}
+}
+
+int main() {
+	init();
+	dfs(1, 0);
+	cout << ans;
 }
